f8046d087940609004cb8fbbafea816ca8605f16
Mihai Negrisan <mihainegrisan1@gmail.com>
2021-12-18 12:48:22 +0200

    Added OrderDate-NumberOfItemsOrdered statistic

---
+++ Project_Manila.DAL/ViewModels/OrderAndItemsGroup.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Manila.DAL.ViewModels
+{
+    public class OrderAndItemsGroup
+    {
+        [DataType(DataType.Date)]
+        public DateTime? OrderDate { get; set; }
+        public int ItemsPerOrder { get; set; }
+    }
+}
--- Project_Manila.Web/Controllers/HomeController.cs
+++ Project_Manila.Web/Controllers/HomeController.cs
@@ -6,21 +6,40 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Project_Manila.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using Project_Manila.DAL;
+using Project_Manila.DAL.ViewModels;
 
 namespace Project_Manila.Web.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly ProjectManilaDBContext _db;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, ProjectManilaDBContext db)
         {
             _logger = logger;
+            _db = db;
         }
 
         public IActionResult Index()
         {
             return View();
+        }
+
+        public async Task<ActionResult> Statistics()
+        {
+            IQueryable<OrderAndItemsGroup> data = _db.Orders
+                .Include(o => o.OrderItems)
+                .OrderBy(o => o.OrderDate)
+                .Select(o => new OrderAndItemsGroup()
+                {
+                    OrderDate = o.OrderDate,
+                    ItemsPerOrder = o.OrderItems.Count
+                });
+
+            return View(await data.AsNoTracking().ToListAsync());
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
